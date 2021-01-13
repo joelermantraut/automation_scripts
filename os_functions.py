d7b11d90829f3ac6c9379118eb117c63c8c0f16b
Joel Ermantraut <joelermantraut@gmail.com>
2021-01-13 20:33:29 -0300

    New files with specifications to develop

---
+++ os_functions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+This class simplifies the use of some os functions
+like run a command, and get the output, etc.
+
+Useful submodules will be os, subprocess, psutil.
+
+"""
---
+++ regex.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+Este archivo va a incluir una clase que 
+facilite el uso de expresiones regulares.
+
+La idea es que no se tenga que diferencia
+entre search, match y findall, y que se
+puedan definir funciones mas simples que
+.group o .groups para obtener los resultados,
+como puede ser la devolucion de listas o
+diccionarios.
+
+Tambien se puede evaluar la definicion de
+expresiones regulares mediante funciones
+concatenadas, pero esto puede no ser
+eficiente, ademas requerira un dominio
+solido del tema.
+
+"""
