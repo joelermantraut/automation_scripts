2f6f755b5e24e8fcc167de28064411a69f2b2c47
Joel Ermantraut <joelermantraut@gmail.com>
2021-01-14 17:29:04 -0300

    Minor changes

--- os_functions.py
+++ os_functions.py
@@ -5,6 +5,9 @@
 This class simplifies the use of some os functions
 like run a command, and get the output, etc.
 
-Useful submodules will be os, subprocess, psutil.
+Useful submodules will be os, subprocess, psutil,
+screeninfo.
 
 """
+
+
