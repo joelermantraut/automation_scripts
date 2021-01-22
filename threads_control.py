08d163422030dd0da865f63fcf404fc577031a46
Joel Ermantraut <joelermantraut@gmail.com>
2021-01-22 17:15:01 -0300

    Added multiprocess function

--- threads_control.py
+++ threads_control.py
@@ -12,20 +12,19 @@ import multiprocessing.dummy
 import threading
 
 class ThreadControl(object):
-    """Class to control multipl threads"""
+    """Class to control multiple threads"""
     def __init__(self):
         self.init()
 
     def init(self):
         pass
 
-    def multiprocess(self, tasks, args, n_processes):
+    def multiprocess(self, function, args, n_processes):
         """
         Creates a shared process.
         """
-
         with multiprocessing.Pool(n_processes) as p:
-            result = p.map(f, args)
+            result = p.map(function, args)
 
         return result
 
@@ -56,22 +55,16 @@ class ThreadControl(object):
         t_p.close()
         return result
 
-    def f(self):
-        print("ok")
-
     def async_thread(self, function, args):
-        self.thread = threading.Thread(self.f)
+        self.thread = threading.Thread(target=function, args=args)
         self.thread.start()
 
-def hola(x):
+def power(x):
     return x**2
 
 def main():
     thread_control = ThreadControl()
-    text = thread_control.run_thread(hola, [2, 4, 6])
-    text2 = thread_control.run_process(hola, [2, 4, 6])
-    print(text)
-    print(text2)
+    thread_control.async_thread(power, [2])
 
 if __name__ == "__main__":
     main()
