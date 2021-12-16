1359c75073282a27cff94b83e0d2b445a4015a99
joelermantraut <joelermantraut@gmail.com>
2021-12-16 17:50:40 -0300

    Modified support for threads

--- threads_control.py
+++ threads_control.py
@@ -5,66 +5,120 @@ Class that simplifies the use of multiple
 threads, asynchronous, and with the ability
 to have a list of which of them are alive
 and to kill them.
+
+The idea for this module is to have several classes
+to control processes and threads.
+
+One class must be to have a process stoppable.
+Another one to control multiple threads automatically.
+Other class would give information about system, useful
+to decide how many threads to use.
+
+Really useful article to understand use of threads and processes.
+https://www.toptal.com/python/beginners-guide-to-concurrency-and-parallelism-in-python#:~:text=What's%20the%20difference%20between%20Python,child%20processes%20bypassing%20the%20GIL.
 """
 
-import multiprocessing
-import multiprocessing.dummy
 import threading
+from queue import Queue
+from time import sleep
+import os
 
-class ThreadControl(object):
-    """Class to control multiple threads"""
-    def __init__(self):
+
+class StoppableThread(threading.Thread):
+    """
+    Thread that can be controlled during execution.
+    """
+    def __init__(self, queue=None, *args, **kwargs):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.args = args
+        self.kwargs = kwargs
+        self._stop_event = threading.Event()
+
+    def run(self):
+        self.target = self.kwargs['target']
+        try:
+            self.target()
+        finally:
+            if self.queue:
+                self.queue.task_done()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+
+class MultipleThreads():
+    """
+    Class to control multiple threads with queues.
+
+    Does a task using multiple threads, taking advantage
+    of multiple cores in a processor.
+    """
+    def __init__(self, n_threads, *args, **kwargs):
+        self.n_threads = n_threads
+        # Number of threads to run
+        self.queue = Queue()
+        self.args = args
+        self.kwargs = kwargs
         self.init()
 
     def init(self):
+        """
+        Runs all threads.
+        """
+        for x in range(self.n_threads):
+            thread = StoppableThread(self.queue, *self.args, **self.kwargs)
+            # Setting daemon to True will let the main thread exit
+            # even though the workers are blocking
+            thread.daemon = True
+            thread.start()
+
+            self.queue.put((thread))
+
+    def stop_all(self):
+        """
+        Stops all threads.
+        """
+        for x in range(self.queue.qsize()):
+            self.stop()
+
+    def stop(self):
+        """
+        Stops one thread in each call.
+        """
+        thread = self.queue.get()
+        thread.stop()
+
+
+class ThreadSystemInfo():
+    """
+    Collection of functions in a class to get info about
+    system cores and processes.
+    """
+    def __init__(self):
         pass
 
-    def multiprocess(self, function, args, n_processes):
-        """
-        Creates a shared process.
-        """
-        with multiprocessing.Pool(n_processes) as p:
-            result = p.map(function, args)
+    def cpu_count(self):
+        return os.cpu_count()
 
-        return result
 
-    def run_tasks(self, function, args, pool, chunk_size=None):
-        """
-        Run tasks receiving the pool to use.
-        """
-        results = pool.map(function, args, chunk_size)
-        return results
-
-    def run_process(self, function, numbers):
-        """
-        Runs a process pool.
-        """
-        p_p = multiprocessing.Pool()
-        result = self.run_tasks(function, numbers, p_p)
-
-        p_p.close()
-        return result
-
-    def run_thread(self, function, numbers):
-        """
-        Runs a thread pool.
-        """
-        t_p = multiprocessing.dummy.Pool()
-        result = self.run_tasks(function, numbers, t_p)
-
-        t_p.close()
-        return result
-
-    def async_thread(self, function, args):
-        self.thread = threading.Thread(target=function, args=args)
-        self.thread.start()
-
-def power(x):
-    return x**2
+def message():
+    print("Message")
 
 def main():
-    thread_control = ThreadControl()
-    thread_control.async_thread(power, [2])
+    thread_control = StoppableThread(target=message)
+    thread_control.start()
+    sleep(2)
+    thread_control.stop()
+
+    multiple_threads = MultipleThreads(2, target=message)
+    sleep(2)
+    multiple_threads.stop_all()
+
+    print(ThreadSystemInfo().cpu_count())
 
 if __name__ == "__main__":
     main()
